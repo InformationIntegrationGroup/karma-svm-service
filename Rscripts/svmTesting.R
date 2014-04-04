3895193cd48df3fc27f20a058d7343938fb885a8
Shrikanth Narayanan <nara471@usc.edu>
2014-04-04 12:35:45 -0700

    udpated svm Testing R script now adding the prediction to the data set in a new column writing the prediction file writing the confusion matrix to a file accepting new args for predictionFileName and matrixFileName

--- Rscripts/svmTesting.R
+++ Rscripts/svmTesting.R
@@ -1,12 +1,34 @@
 #Testing part
 library(e1071)
+
 args<-commandArgs(TRUE)
+args
 model <- load(args[2])
-testset<-read.csv(args[1]) #first argument URL
-ncol(testset)
-svm.pred <- predict(svmModel, testset[,-ncol(testset)])
-confusionMatrix<-table(pred = svm.pred, true = testset[,ncol(testset)])
-plot(svm.pred)
-confusionMatrix
+predictedFileName <- args[3]
+confusionMatrixFile <- args[4]
+
+testset<-read.csv(args[1],head=T,sep=',') #first argument URL
+numOfcol<-ncol(testset)
+
+x<-testset[,-numOfcol]
+y<-testset[,numOfcol]
+
+svm.pred <- predict(svmModel, x)
+
+testset['predicted'] <- fitted(svmModel)
+#testset[1:8,]
+
+confusionMatrix <- table(fitted(svmModel), y)
+pred <- predict(svmModel, x, decision.values = TRUE)
+
+#summary(svmModel)
+#attr(pred, "decision.values")[1:8,]
+
+write.table(testset, predictedFileName, sep=',', quote=TRUE)
+
+#plot(svm.pred)
+
+write.table(confusionMatrix,file=confusionMatrixFile,sep='\t')
+
 accuracy <- paste("Accuracy: ",classAgreement(confusionMatrix)$diag,sep=" ")
 accuracy
